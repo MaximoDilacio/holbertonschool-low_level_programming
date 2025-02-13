353888715d231d42c26d1f765cfe44ee894198f0
MaximoDilacio <developermaximo1@gmail.com>
2025-02-13 22:38:34 +0000

    File 4 fiifififix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void) /* funcion puts para mostrar frase */
+/**
+*Funcion principal
+*Imprime un mensaje de salida 
+*Este programa utiliza la funcion Puts()
+*/
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
