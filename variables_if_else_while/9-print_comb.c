4936de8d497ff1a438b55dd00b4637ff1ac14d67
MaximoDilacio <developermaximo1@gmail.com>
2025-02-17 13:33:59 +0000

    File 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Funcion Principal
+ * Return: Retorna 0 si se ejecuta correctamente
+*/
+int main(void)
+{
+	char numero;
+
+	for (numero = '0'; numero <= '9'; numero++)
+	{
+		if (numero != '9')
+		{
+			putchar(numero);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
