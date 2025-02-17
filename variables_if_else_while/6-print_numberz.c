da328e5ebd607b1473fe02877f33d0ec926cf852
MaximoDilacio <developermaximo1@gmail.com>
2025-02-17 13:15:15 +0000

    File 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Funcion Principal
+ * Return: Retorna 0 si se ejecuta correctamente
+*/
+int main(void)
+{
+	int numero;
+
+	for (numero = 0; numero <= 9; numero++)
+	{
+		putchar(numero);
+	}
+	putchar('\n');
+	return (0);
+}
