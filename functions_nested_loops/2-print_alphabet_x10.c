c67660d223040d73fa8160352f19891a528c355f
MaximoDilacio <developermaximo1@gmail.com>
2025-02-18 13:51:52 +0000

    File fixed 0202

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - print alphabet
- */
+ * print_alphabet_x10 - print alphabet
+*/
 void print_alphabet_x10(void)
 {
 	char letra;
 	char i;
 
-	for (i = 0; i <10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (letra = 'a'; letra <= 'z'; letra++)
-		{	 
-		_putchar(letra);
-        	}
+		{
+			_putchar(letra);
+		}
 		_putchar('\n');
-	}	
-
-}
+		}
+	}
