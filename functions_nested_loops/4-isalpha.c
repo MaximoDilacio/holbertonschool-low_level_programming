e7a18b10ef744cdcbc4198a319bd386fd8da8905
MaximoDilacio <developermaximo1@gmail.com>
2025-02-18 14:09:51 +0000

    File 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+int _isalpha(int c)
+{
+	if(isalpha(c))
+	{
+		return (1);	
+	}
+
+	return (0);
+}
