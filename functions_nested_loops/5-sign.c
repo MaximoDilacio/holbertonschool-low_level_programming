f4bf68781e0bc6170474679ddd4de553ce0c9e58
MaximoDilacio <developermaximo1@gmail.com>
2025-02-18 14:26:58 +0000

    File fix 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - check if number is positive or negative
+ *
+ * @n: parameter that obtain a number
+ *Return: 1 if is positive, 0 if zero, -1 if negative
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
