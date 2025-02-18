119e666d5224681118b298a63547a40bdc8a6643
MaximoDilacio <developermaximo1@gmail.com>
2025-02-18 14:34:19 +0000

    File fix 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_abs - print abs of a number
+ *
+ *@n: parameter that obtain a number
+ *Return: n if is positive or zero, -n if is negative
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+
+	return (n);
+}
