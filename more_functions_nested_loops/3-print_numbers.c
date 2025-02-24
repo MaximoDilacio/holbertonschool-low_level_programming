d7df0504fd9434741262b94dcacc9a5e96451f16
MaximoDilacio <developermaximo1@gmail.com>
2025-02-24 17:21:43 +0000

    File fix 03

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- *
- *
+ * print_numbers - Function print numbers 0/9
 */
 void print_numbers(void)
 {
@@ -10,6 +9,5 @@ void print_numbers(void)
 	for (n = 48; n <= 57; n++)
 	{
 		_putchar(n);
-		_putchar('\n');
 	}
 }
