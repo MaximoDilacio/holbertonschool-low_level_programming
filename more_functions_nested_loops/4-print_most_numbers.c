3b3e3ba7f0e9a0cb4d00b380b878ef0039e82b96
MaximoDilacio <developermaximo1@gmail.com>
2025-02-24 17:36:58 +0000

    File 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_most_numbers - print numbers !2 && !4
+*/
+void print_most_numbers(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		if (n != 50 && n != 52)
+		{
+			_putchar(n);
+		}
+	}
+	printf("\n");
+}
