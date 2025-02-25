4b549586b6e12e6f66f2cf0e3d0696bce7c8bb30
MaximoDilacio <developermaximo1@gmail.com>
2025-02-25 13:06:12 +0000

    File fix 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - function print /
+ * @size: parameter is number
+*/
+void print_square(int size)
+{
+	int f;
+	int c;
+
+	while (c <= size)
+	{
+		for (f = 0; f < size; f++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		c++;
+	}
+}
