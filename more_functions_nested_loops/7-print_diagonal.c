2e60abf34a73c66bf59c1d3ff9faa0c82c211d0a
MaximoDilacio <developermaximo1@gmail.com>
2025-02-25 12:37:42 +0000

    File fix 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,25 @@
 void print_diagonal(int n)
 {
 	int i = 0;
+	int e;
 
 	if (n > 0)
 	{
 		while (i < n)
 		{
+			for (e = 0; e < i; e++)
+			{
+				_putchar(' ');
+			}
 			_putchar('\\');
 			i++;
+			_putchar('\n');
 		}
-	}
 
-	_putchar('\n');
+		if (i > n)
+		{
+			_putchar('\n');	
+		}
+	
+	}
 }
