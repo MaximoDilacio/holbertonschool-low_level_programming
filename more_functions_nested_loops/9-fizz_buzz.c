d4c0fad9a34407de38b8870817727400a48e5f3b
MaximoDilacio <developermaximo1@gmail.com>
2025-02-25 17:17:43 +0000

    File fix 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,33 @@
 
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - Principal function
+ * Return: Return 0 if execute correctly
 */
 int main(void)
 {
 	int n = 1;
 
-        while (n <= 100)
-        {
+	while (n <= 100)
+	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
 			printf("FizzBuzz");
-		} else if (n % 3 == 0){
+		} else if (n % 3 == 0)
+		{
 			printf("Fizz");
 		} else if (n % 5 == 0)
 		{
 			printf("Buzz");
-		} else 
+		} else
 		{
 			printf("%d", n);
 		}
 		if (n != 100)
-		{	
-                	printf(",");
+		{
+			printf(" ");
 		}
-                printf(" ");
-
-                n++;
-        }
-
+		n++;
+	}
 	return (0);
 }
