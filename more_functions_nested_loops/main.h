61f7122da802064eb8417b7e451700ed25db970b
MaximoDilacio <developermaximo1@gmail.com>
2025-02-26 14:04:31 +0000

    File fix 10

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,6 +2,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int _putchar( char c);
 int mul(int a, int b);
+void print_triangle(int size);
 void print_line(int n);
 void print_square(int size);
 void print_diagonal(int n);
