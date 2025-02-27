18e51285770a3321452af64ef46aa1d3e93dfd5b
MaximoDilacio <developermaximo1@gmail.com>
2025-02-27 12:50:09 +0000

    File fixed 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * _strlen - function that print count caracters
+ * @s: is array
+ * Return: return value n
 */
 int _strlen(char *s)
 {
@@ -11,7 +11,6 @@ int _strlen(char *s)
 
 	while (s[n] != '\0')
 		n++;
-
 	return (n);
 }
 
