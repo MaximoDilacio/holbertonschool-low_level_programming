c6aef59ed27ba2b09bbc8a2103c759a79ba8f453
MaximoDilacio <developermaximo1@gmail.com>
2025-03-05 14:11:45 +0000

    File fix 8

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 int _strlen(char *s);
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
