ee355b67980a260c790086d6d583887fe45989f1
MaximoDilacio <developermaximo1@gmail.com>
2025-03-06 00:12:48 +0000

    File fixed 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 /**
  * _puts_recursion - function that prints a string
  * @s: string
@@ -9,11 +9,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		write(1, "\n", 1);
 		return;
 	}
 
-	putchar(*s);
+	write(1, *s, 1);
 	_puts_recursion(s + 1);
 
 
