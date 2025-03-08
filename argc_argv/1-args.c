ebffdba456ef08f9c19ca29c4eed5553f51de318
MaximoDilacio <developermaximo1@gmail.com>
2025-03-08 22:16:36 +0000

    File 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number og argument passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: return 0
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc - 1);
+	return (0);
+}
