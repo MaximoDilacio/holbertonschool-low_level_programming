966156bea7fe22444498f261e8d8574f3017fb55
MaximoDilacio <developermaximo1@gmail.com>
2025-03-14 12:27:08 +0000

    File fix 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include "main.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * struct dog - Struct that store information of dog
+ * @name: Name dog
+ * @age: Age dog
+ * @owner: Owner dog
 */
 
 typedef struct dog
@@ -12,4 +15,6 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}dog;
+} dog;
+
+#endif
