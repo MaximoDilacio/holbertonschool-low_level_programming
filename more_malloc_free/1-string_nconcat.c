e0ec8d72dd0f926c6ed02bedfb41a4c9508bc2f2
MaximoDilacio <developermaximo1@gmail.com>
2025-03-14 11:44:03 +0000

    File fix 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,43 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
+/**
+ * _strlen - Calculates the length of a string.
+ * @s: The string.
+ *
+ * Return: The length of the string.
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	while (s && s[len])
+		len++;
+
+	return (len);
+}
+
 /**
  * string_nconcat - Concatenates two strings.
  * @s1: First string.
  * @s2: Second string.
  * @n: Number of bytes from s2 to concatenate.
  *
- * Return: Pointer
+ * Return: Pointer to the new allocated string or NULL if fails.
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, i, j;
+	unsigned int len1, len2, i, j;
 	char *result;
 
-	if (s1 != NULL)
-		len1 = strlen(s1);
-	if (s2 != NULL)
-		len2 = strlen(s2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
 	if (n >= len2)
 		n = len2;
 
 	result = malloc(len1 + n + 1);
 	if (result == NULL)
-		return (NULL);
+		exit(1);
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
