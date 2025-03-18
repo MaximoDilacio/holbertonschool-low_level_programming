b2c98380ff861fc68b869c243680ff12138e93b0
MaximoDilacio <developermaximo1@gmail.com>
2025-03-18 12:56:23 +0000

    File 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: pointer
+ * @size: int
+ * @cmp: function
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; array[i] < size; i++)
+		if (cmp(array[i]))
+			return (array[i]);
+
+	return (-1);
+}
