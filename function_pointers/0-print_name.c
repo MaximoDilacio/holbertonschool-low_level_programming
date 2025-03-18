fa1b5656f8abcc65d67b5e2df28ac98cd17144f0
MaximoDilacio <developermaximo1@gmail.com>
2025-03-18 12:20:14 +0000

    File fix 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,11 +2,9 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
- *
+ * print_name - Function that print name
+ * @name: char
+ * @f: function
 */
 void print_name(char *name, void (*f)(char *))
 {
