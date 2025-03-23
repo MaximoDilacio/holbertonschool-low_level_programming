29b08b4def6db6d4ed47ec6df8439c4e645c3966
MaximoDilacio <developermaximo1@gmail.com>
2025-03-23 13:53:08 +0000

    Fix betty :)

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,13 +11,13 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *str, *sep = "";
 
-	va_start(args,format);
+	va_start(args, format);
 
 	while (format && format[i])
 	{
 		printf("%s", sep);
 		sep = ", ";
-		
+
 		switch (format[i])
 		{
 			case 'c':
@@ -39,10 +39,9 @@ void print_all(const char * const format, ...)
 		}
 
 		i++;
-	}	
+	}
 
 	printf("\n");
 	va_end(args);
-
 
 }
