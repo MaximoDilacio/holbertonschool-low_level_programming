893cc1c36bf28a471d3534b1ef17498e03a9cd08
MaximoDilacio <developermaximo1@gmail.com>
2025-04-05 14:38:05 +0000

    File fix compile

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -18,7 +18,8 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-/* Prototipo de función */
+
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* LISTS_H */
