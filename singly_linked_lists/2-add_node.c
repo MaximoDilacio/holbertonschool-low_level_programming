a28df448dd0fe1dfbcecc325b73e6447b8ed8e1d
MaximoDilacio <developermaximo1@gmail.com>
2025-04-10 10:22:05 +0000

    File fix 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,38 +1,37 @@
-#include "lists.h" /* Assuming you have a header file with the list_t definition */
 #include <stdlib.h>
 #include <string.h>
+#include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: Double pointer to the head of the list.
- * @str: String to be duplicated and stored in the new node.
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to duplicate and add in the new node
  *
- * Return: Address of the new element, or NULL if it failed.
+ * Return: Address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
+	list_t *new_node;
+	char *dup_str;
 
-    if (str == NULL)
-    {
-        return (NULL);
-    }
+	if (str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-    {
-        return (NULL);
-    }
+	dup_str = strdup(str);
+	if (dup_str == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(dup_str);
+		return (NULL);
+	}
 
-    new_node->next = *head;
-    *head = new_node;
+	new_node->str = dup_str;
+	new_node->len = strlen(dup_str);
+	new_node->next = *head;
+	*head = new_node;
 
-    return (new_node);
+	return (new_node);
 }
